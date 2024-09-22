79214fc00eb6059b48a89852d054a8aa6a8f0492
TIHeitorDS <heitorclaudinods@gmail.com>
2024-09-22 10:43:37 -0300

    fix: import three

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-import * as THREE from "three";
+import * as THREE from "../../node_modules/three/build/three.module.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
