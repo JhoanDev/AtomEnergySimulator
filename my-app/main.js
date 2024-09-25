bc490f01e4285cc202e1362cd7783ecb0f9c214d
TIHeitorDS <heitorclaudinods@gmail.com>
2024-09-24 21:12:36 -0300

    feat: add animation of core

--- my-app/main.js
+++ my-app/main.js
@@ -1,13 +1,17 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Inicializar OrbitControls
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true; // Suaviza a movimentação
 controls.dampingFactor = 0.25; // Fator de suavização
@@ -23,50 +27,82 @@ const scene = new THREE.Scene();
 
 // Adicionando luz ambiente
 const light = new THREE.AmbientLight(0xffffff, 2);
+const speed = 0.1;
+let time = 0.1;
+
 scene.add(light);
 
-// Nucleo do átomo
-class NuclearModel {
-  constructor() {
+class Atom {
+  constructor(type) {
     this.model = null;
+    this.type = type;
     this.load(this);
+  }
+
+  move(direction, value) {
+    time += speed;
+
+    if (direction == "x" && this.model) {
+      this.model.position.x = Math.sin(time) * speed + value;
+    } else if (direction == "y" && this.model) {
+      this.model.position.y = Math.sin(time) * speed + value;
+    } else if (direction == "z" && this.model) {
+      this.model.position.z = Math.sin(time) * speed + value;
+    }
   }
 
   load(object) {
     const loader = new GLTFLoader();
-    loader.load(
-      'models/core.gltf',
-      function (gltf) {
-        scene.add(gltf.scene);
-        object.model = gltf.scene.children[0];
-        gltf.animations; // Array<THREE.AnimationClip>
-        gltf.scene; // THREE.Group
-        gltf.scenes; // Array<THREE.Group>
-        gltf.cameras; // Array<THREE.Camera>
-        gltf.asset; // Object
-      },
-      function (xhr) {
-        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
-      },
-      function (error) {
-        console.error('An error happened:', error);
-      }
-    );
+    loader.load(`src/models/core/${this.type}.gltf`, (gltf) => {
+      scene.add(gltf.scene);
+
+      object.model = gltf.scene.children[0];
+
+      gltf.animations;
+      gltf.scene;
+      gltf.scenes;
+      gltf.cameras;
+      gltf.asset;
+    });
   }
 
   rotate() {
     if (this.model) {
-      this.model.rotation.x += 0.01; // Rotação ao longo do eixo X
-      this.model.rotation.y += 0.01; // Rotação ao longo do eixo Y
+      this.model.rotation.x += 0.01;
+      this.model.rotation.y += 0.01;
     }
   }
 }
 
-const nucleo = new NuclearModel();
+const neutrons = [
+  new Atom("neutron"),
+  new Atom("neutron"),
+  new Atom("neutron"),
+  new Atom("neutron"),
+];
+
+const protons = [new Atom("proton"), new Atom("proton"), new Atom("proton")];
 
 function animate() {
-  controls.update(); // Atualiza os controles
-  nucleo.rotate(); // Rotaciona o modelo
+  controls.update();
+
+  protons.forEach((proton) => {
+    proton.rotate();
+  });
+
+  neutrons.forEach((neutron) => {
+    neutron.rotate();
+  });
+
+  neutrons[0].move("y", 0);
+  neutrons[1].move("x", -2);
+  neutrons[2].move("y", -2);
+  neutrons[3].move("z", -2);
+
+  protons[0].move("x", 2);
+  protons[1].move("y", 2);
+  protons[2].move("z", 2);
+
   renderer.render(scene, camera);
 }
 
